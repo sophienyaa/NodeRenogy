62e8ffd6eea60300cf418a452a217ae9ab4b5ff6
Sophie Wheeler <me@sophi.ee>
2021-10-12 16:52:05 +0100

    fix mqtt

--- mqtt.js
+++ mqtt.js
@@ -2,7 +2,6 @@ const cli = require('./cli');
 const mqtt = require('async-mqtt');
 const logger = require('./logger');
 const args = cli.args;
-const client;
 
 const mqttOptions = {
     username: args.mqttuser,
@@ -10,16 +9,18 @@ const mqttOptions = {
     clientId: `nodeRenogy_${Math.random().toString(16).substr(2,8)}`    
 };
 
+logger.trace('Connecting to MQTT broker...');
+logger.trace(mqttOptions, 'With MQTT options...');
+const client = await mqtt.connectAsync(`tcp://${args.mqttbroker}`, mqttOptions)
+
+if(client.connected) {
+    logger.info('Connected to MQTT Broker!');
+}
+
 module.exports = {
-    begin: async function() {
-        logger.trace('Connecting to MQTT broker...');
-        logger.trace(mqttOptions, 'With MQTT options...');
-        client = await mqtt.connectAsync(`tcp://${args.mqttbroker}`, mqttOptions)
-        logger.info('Connected to MQTT Broker!')
-    },
     publish: async function(data) {
         try {
-            logger.trace('Publishing data to MQTT...');
+            logger.trace('Publishing to MQTT!');
             await client.publish(`${args.mqtttopic}/state`, JSON.stringify(data));
             await client.end();
         } catch (e){
